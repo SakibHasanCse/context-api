bac0786abf48c3bbd51f74946a403dd1aaac9e0e
SakibHasanCse <sakibhasan9641@gmail.com>
2021-07-11 23:47:18 +0600

    add: setup

---
+++ reactapp/src/contexts/userProvider.js
@@ -0,0 +1,17 @@
+
+import React, { useState } from 'react'
+
+
+
+const UserProvider = ({ children }) => {
+    const [name, setName] = useState('Sakib hasan');
+    const [age, setAge] = useState(20);
+    const happyBirthday = () => setAge(age + 1);
+
+
+    return (
+        <UserContext.Provider value={name, age, happyBirthday}>
+            {children}
+        </UserContext.Provider>
+    )
+}
